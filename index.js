61134d921ee53d71966cf5e26336c6f8d6c7c393
Gary <garros2703@students.ecpi.edu>
2022-08-23 07:37:45 -0500

    commit tree 2bb012f53dcda11c446f0a6cb97c3c54a692c5ad

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+    return string.toUpperCase('hello');
+}
+
+function whisper(string) {
+    return string.toLowerCase('HELLO');
+}
+
+function logShout(string) {
+    console.log('HELLO');
+}
+function logWhisper(string) {
+    console.log('hello');
+}
+function sayHiToHeadphonedRoommate(string) {
+    return string.toLowerCase('I can\'t hear you!');
+}
